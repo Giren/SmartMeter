2bf92897c211aaafb02b2fe4e91661e136e8e808
Michi <giren@web.de>
2015-12-19 16:20:32 +0100

    alle saves drinne

---
+++ mobile/src/main/java/com/moc/smartmeterapp/database/DatabaseHelper.java
@@ -0,0 +1,66 @@
+package com.moc.smartmeterapp.database;
+
+import com.moc.smartmeterapp.model.Day;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by philipp on 16.12.2015.
+ */
+public class DatabaseHelper implements IDatabase {
+    @Override
+    public void createIfNotCreated() {
+        
+    }
+
+    @Override
+    public Day loadDay(Date date) {
+        return null;
+    }
+
+    @Override
+    public List<Day> loadMonth(Date date) {
+        return null;
+    }
+
+    @Override
+    public List<Day> loadYear(Date date) {
+        return null;
+    }
+
+    @Override
+    public void saveDay(Day day) {
+
+    }
+
+    @Override
+    public void saveMonth(List<Day> days) {
+
+    }
+
+    @Override
+    public void saveYear(List<Day> days) {
+
+    }
+
+    @Override
+    public void deleteDay(Date date) {
+
+    }
+
+    @Override
+    public void deleteMonth(Date date) {
+
+    }
+
+    @Override
+    public void deleteYear(Date date) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+}
